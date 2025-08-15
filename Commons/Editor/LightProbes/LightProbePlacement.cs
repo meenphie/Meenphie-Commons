175b53b664050906030974fe5960bbd2f3f83eb0
Meenphie <hello@meenphie.com>
2025-08-15 19:13:56 +0200

    Update to LightProbePlacement

--- Commons/Editor/LightProbes/LightProbePlacement.cs
+++ Commons/Editor/LightProbes/LightProbePlacement.cs
@@ -3,149 +3,146 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Collections;
 
-public class LightProbePlacement : EditorWindow {
+public class LightProbePlacement : EditorWindow
+{
+    static float progress = 0.0f;
+    static string current = "Hello";
+    static bool working = false;
 
-	static float progress = 0.0f;
-	static string current = "Hello";
-	static bool working = false;
+    float mergeDistance = 1;
+    LightProbeGroup probeObject; // Changed from GameObject to LightProbeGroup
+    bool disableMerging;
 
-	float mergeDistance = 1;
-	GameObject probeObject;
-	bool disableMerging;
+    [MenuItem("Window/Generate Light Probes")]
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(LightProbePlacement));
+        window.Show();
+    }
 
-	[MenuItem ("Window/Generate Light Probes")]
-	static void Init() {
-		EditorWindow window = GetWindow (typeof(LightProbePlacement));
-		window.Show ();
-	}
+    void PlaceProbes()
+    {
+        LightProbeGroup p = probeObject;
+        if (p != null)
+        {
+            working = true;
 
-	void PlaceProbes () {
-		GameObject probe = probeObject;
-		if(probe != null) {
-			LightProbeGroup p = probe.GetComponent<LightProbeGroup>();
+            progress = 0.0f;
+            current = "Triangulating navmesh...";
+            EditorUtility.DisplayProgressBar("Generating probes", current, progress);
 
-			if(p != null) {
-					
-				working = true;
+            p.transform.position = Vector3.zero;
 
-				progress = 0.0f;
-				current = "Triangulating navmesh...";
-				EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
+            UnityEngine.AI.NavMeshTriangulation navMesh = UnityEngine.AI.NavMesh.CalculateTriangulation();
 
+            current = "Generating necessary lists...";
+            EditorUtility.DisplayProgressBar("Generating probes", current, progress);
 
-				probe.transform.position = Vector3.zero;
+            Vector3[] newProbes = navMesh.vertices;
+            List<Vector3> probeList = new List<Vector3>(newProbes);
+            List<ProbeGenPoint> probeGen = new List<ProbeGenPoint>();
 
-				UnityEngine.AI.NavMeshTriangulation navMesh = UnityEngine.AI.NavMesh.CalculateTriangulation ();
+            foreach (Vector3 pg in probeList)
+            {
+                probeGen.Add(new ProbeGenPoint(pg, false));
+            }
 
+            List<Vector3> mergedProbes = new List<Vector3>();
+            int probeListLength = newProbes.Length;
+            int done = 0;
 
-				current = "Generating necessary lists...";
-				EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
+            foreach (ProbeGenPoint pro in probeGen)
+            {
+                if (!pro.used)
+                {
+                    current = "Checking point at " + pro.point.ToString();
+                    progress = (float)done / probeListLength;
+                    EditorUtility.DisplayProgressBar("Generating probes", current, progress);
 
-				Vector3[] newProbes = navMesh.vertices;
-				List<Vector3> probeList = new List<Vector3>(newProbes);
-				List<ProbeGenPoint> probeGen = new List<ProbeGenPoint>();
+                    List<Vector3> nearbyProbes = new List<Vector3> { pro.point };
+                    pro.used = true;
 
-				foreach(Vector3 pg in probeList) {
-					probeGen.Add (new ProbeGenPoint(pg, false));
-				}
+                    if (!disableMerging)
+                    {
+                        foreach (ProbeGenPoint pp in probeGen)
+                        {
+                            if (!pp.used)
+                            {
+                                if (Vector3.Distance(pp.point, pro.point) <= mergeDistance)
+                                {
+                                    pp.used = true;
+                                    nearbyProbes.Add(pp.point);
+                                }
+                            }
+                        }
+                    }
 
-				EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
+                    Vector3 newProbe = Vector3.zero;
+                    foreach (Vector3 prooo in nearbyProbes)
+                    {
+                        newProbe += prooo;
+                    }
+                    newProbe /= nearbyProbes.Count;
+                    newProbe += Vector3.up;
 
-				List<Vector3> mergedProbes = new List<Vector3>();
+                    mergedProbes.Add(newProbe);
+                    done += 1;
+                }
+            }
 
-				int probeListLength = newProbes.Length;
+            current = "Final steps...";
+            EditorUtility.DisplayProgressBar("Generating probes", current, progress);
 
-				int done = 0;
-				foreach(ProbeGenPoint pro in probeGen) {
-					if(pro.used == false) {
-						current = "Checking point at " + pro.point.ToString ();
-						progress = (float)done / (float)probeListLength;
-						EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
-						List<Vector3> nearbyProbes = new List<Vector3>();
-						nearbyProbes.Add (pro.point);
-						pro.used = true;
-						if (!disableMerging) {
-							foreach(ProbeGenPoint pp in probeGen) {
-								if(pp.used == false) {
-									current = "Checking point at " + pro.point.ToString ();
-									//EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
-									if(Vector3.Distance (pp.point, pro.point) <= mergeDistance) {
-										pp.used = true;
-										nearbyProbes.Add (pp.point);
-									}
-								}
-							}
-						}
+            p.probePositions = mergedProbes.ToArray();
 
-						Vector3 newProbe = new Vector3();
-						foreach(Vector3 prooo in nearbyProbes) {
-							newProbe += prooo;
-						}
-						newProbe /= nearbyProbes.ToArray ().Length;
-						newProbe += Vector3.up;
+            working = false;
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error", "Probe object not set", "OK");
+        }
+    }
 
-						mergedProbes.Add (newProbe);
-						done += 1;
-						//Debug.Log ("Added probe at point " + newProbe.ToString ());
-					}
-				}
+    void OnGUI()
+    {
+        if (GUILayout.Button("Generate probes"))
+        {
+            PlaceProbes();
+        }
 
-				/*for(int i=0; i<newProbes.Length; i++) {
-					newProbes[i] = newProbes[i] + Vector3.up;
-				}*/
+        mergeDistance = EditorGUILayout.FloatField("Vector merge distance", mergeDistance);
+        disableMerging = EditorGUILayout.Toggle("Disable merging", disableMerging);
+        probeObject = (LightProbeGroup)EditorGUILayout.ObjectField("Light Probe Group", probeObject, typeof(LightProbeGroup), true);
 
+        EditorGUILayout.LabelField("This script will automatically generate light probe positions based on the current navmesh.");
+        EditorGUILayout.LabelField("Please make sure that you have generated a navmesh before using the script.");
+        EditorGUILayout.LabelField("If your navmesh is very large or complex, try using 'Disable Merging' to tremendously speed up the process.");
+        EditorGUILayout.LabelField("Keep in mind this may produce more probes than necessary, which may negatively impact performance.");
 
-				current = "Final steps...";
-				EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
-				
-				p.probePositions = mergedProbes.ToArray ();
-				EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
+        if (working)
+        {
+            EditorUtility.DisplayProgressBar("Generating probes", current, progress);
+        }
+        else
+        {
+            EditorUtility.ClearProgressBar();
+        }
+    }
 
-				working = false;
-
-
-			} else {
-				EditorUtility.DisplayDialog("Error", "Probe object does not have a Light Probe Group attached to it", "OK");
-			}
-
-		} else {
-			EditorUtility.DisplayDialog("Error", "Probe object not set", "OK");
-		}
-	}
-
-	void OnGUI() {
-
-		if(GUILayout.Button("Generate probes")) {
-			PlaceProbes ();
-		}
-		mergeDistance = EditorGUILayout.FloatField ("Vector merge distance",mergeDistance);
-		disableMerging = EditorGUILayout.Toggle ("Disable merging", disableMerging);
-		probeObject = (GameObject)EditorGUILayout.ObjectField ("Probe GameObject" , probeObject, typeof(GameObject), true);
-		EditorGUILayout.LabelField ("This script will automatically generate light probe positions based on the current navmesh.");
-		EditorGUILayout.LabelField ("Please make sure that you have generated a navmesh before using the script.");
-		EditorGUILayout.LabelField ("If your navmesh is very large or complex, try using 'Disable Merging' to tremendously speed up the process. Keep in mind this may produce more probes than necessary, which may negatively impact performance.");
-
-		if(working) {
-			EditorUtility.DisplayProgressBar ("Generating probes", current, progress);
-		} else {
-			EditorUtility.ClearProgressBar ();
-		}
-	}
-
-	void OnInspectorUpdate() {
-		Repaint();
-	}
-
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
 }
 
-public class ProbeGenPoint {
+public class ProbeGenPoint
+{
+    public Vector3 point;
+    public bool used = false;
 
-	public Vector3 point;
-	public bool used = false;
-
-	public ProbeGenPoint(Vector3 p, bool u) {
-		point = p;
-		used = u;
-	}
-
+    public ProbeGenPoint(Vector3 p, bool u)
+    {
+        point = p;
+        used = u;
+    }
 }
