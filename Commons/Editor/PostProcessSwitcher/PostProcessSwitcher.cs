822889882c3262e802492f68a1d131a4a1d1f089
Meenphie <hello@meenphie.com>
2025-09-11 20:22:37 +0200

    Fix minor issue with post process switcher

--- Commons/Editor/PostProcessSwitcher/PostProcessSwitcher.cs
+++ Commons/Editor/PostProcessSwitcher/PostProcessSwitcher.cs
@@ -1,43 +1,49 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEngine.Rendering.PostProcessing;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
 
-[InitializeOnLoad]
-public class PostProcessingInitializer
+public class PostProcessingInitializer : IActiveBuildTargetChanged
 {
-    static PostProcessingInitializer()
+    // Required by IActiveBuildTargetChanged
+    public int callbackOrder => 0;
+
+    // Called whenever the active build target changes
+    public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget)
     {
         UpdatePostProcessingSettings();
     }
 
     private static void UpdatePostProcessingSettings()
     {
-        var postProcessLayers = Object.FindObjectsOfType<PostProcessLayer>(true);
+        var sceneDescriptor = Object.FindObjectOfType<VRCSceneDescriptor>(true);
+        var refCam = sceneDescriptor.ReferenceCamera;
+        var postProcessLayer = refCam.GetComponent<PostProcessLayer>();
         var postProcessVolumes = Object.FindObjectsOfType<PostProcessVolume>(true);
 
-#if UNITY_ANDROID || UNITY_IOS
-        SetPostProcessingSettings(false, "EditorOnly", postProcessLayers, postProcessVolumes);
-        Debug.Log("Post-processing is: <color=red>Disabled</color>");
+#if UNITY_ANDROID
+        SetPostProcessingSettings(false, "EditorOnly", postProcessLayer, postProcessVolumes);
+        Debug.Log($"[<color=purple>Meenphie</color>] Post-processing Disabled");
 #else
-        SetPostProcessingSettings(true, "Untagged", postProcessLayers, postProcessVolumes);
-        Debug.Log("Post-processing is: <color=green>Enabled</color>");
+        SetPostProcessingSettings(true, "Untagged", postProcessLayer, postProcessVolumes);
+        Debug.Log($"[<color=purple>Meenphie</color>] Post-processing Enabled");
 #endif
     }
 
-    private static void SetPostProcessingSettings(bool enable, string tag, PostProcessLayer[] layers, PostProcessVolume[] volumes)
+    private static void SetPostProcessingSettings(bool enable, string tag, PostProcessLayer layer, PostProcessVolume[] volumes)
     {
-        foreach (var layer in layers)
+        if (layer != null)
         {
-            if (layer.enabled != enable || layer.tag != tag)
-            {
-                layer.tag = tag;
-                layer.enabled = enable;
-            }
+            layer.tag = tag;
+            layer.enabled = enable;
         }
 
         foreach (var volume in volumes)
         {
-            if (volume.tag != tag)
+            if (volume != null && volume.tag != tag)
             {
                 volume.tag = tag;
             }
