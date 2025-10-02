67b47aae256bb93304d317a442029cb64a4cd9e9
Meenphie <hello@meenphie.com>
2025-10-02 12:43:41 +0200

    Added LOD colliders tool

--- Commons/Editor/LODTools/LODTools.cs
+++ Commons/Editor/LODTools/LODTools.cs
@@ -3,7 +3,11 @@ using UnityEditor;
 
 public static class LODTools
 {
-    [MenuItem("Meenphie/Set LODs")]
+    private const float LOD0_HEIGHT = 0.5f;
+    private const float LOD1_HEIGHT = 0.25f;
+    private const float LAST_LOD_HEIGHT = 0.05f;
+
+    [MenuItem("Meenphie/LOD/Set LODs Distances")]
     private static void SetLods()
     {
         var lodGroups = Object.FindObjectsOfType<LODGroup>();
@@ -23,17 +27,17 @@ public static class LODTools
                 lodArray[i] = lods[i];
                 float targetHeight = lodArray[i].screenRelativeTransitionHeight;
 
-                if (i == lods.Length - 1)
+                if (i == 0)
                 {
-                    targetHeight = 0.05f; // Last LOD
-                }
-                else if (i == 0)
-                {
-                    targetHeight = 0.5f; // First LOD
+                    targetHeight = LOD0_HEIGHT;
                 }
                 else if (i == 1)
                 {
-                    targetHeight = 0.25f; // Second LOD
+                    targetHeight = LOD1_HEIGHT;
+                }
+                else if (i == lods.Length - 1)
+                {
+                    targetHeight = LAST_LOD_HEIGHT;
                 }
 
                 if (!Mathf.Approximately(lodArray[i].screenRelativeTransitionHeight, targetHeight))
@@ -57,5 +61,40 @@ public static class LODTools
             Debug.Log($"[<color=purple>Meenphie</color>] Updated LODs for {count} LODGroup(s).");
         else
             Debug.Log("[<color=purple>Meenphie</color>] No LODs needed updating.");
+    }
+
+    [MenuItem("Meenphie/LOD/Remove Extra Colliders")]
+    private static void RemoveCollidersFromNonLOD0()
+    {
+        var lodGroups = Object.FindObjectsOfType<LODGroup>();
+        int removedCount = 0;
+
+        foreach (var lodGroup in lodGroups)
+        {
+            var lods = lodGroup.GetLODs();
+            if (lods.Length <= 1)
+                continue;
+
+            // Skip LOD0, process LOD1+
+            for (int i = 1; i < lods.Length; i++)
+            {
+                foreach (var renderer in lods[i].renderers)
+                {
+                    if (renderer == null) continue;
+
+                    var colliders = renderer.GetComponentsInChildren<Collider>(true);
+                    foreach (var collider in colliders)
+                    {
+                        Undo.DestroyObjectImmediate(collider);
+                        removedCount++;
+                    }
+                }
+            }
+        }
+
+        if (removedCount > 0)
+            Debug.Log($"[<color=purple>Meenphie</color>] Removed {removedCount} Colliders.");
+        else
+            Debug.Log("[<color=purple>Meenphie</color>] No Colliders found outside LOD0.");
     }
 }
